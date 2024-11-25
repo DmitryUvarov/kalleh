2e206e3c2f0629db49a70d2f73ce3ea73ff73abc
Dima <uvarov3123@gmail.com>
2024-11-25 19:36:41 +0200

    update counter

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -9,36 +9,39 @@ function pageLoad() {
   const htmlTag = document.documentElement
 
   const counter = document.querySelector(".counter")
-  const timeTo = counter.getAttribute("data-timeTo")
-  const targetDate = new Date(timeTo) // Преобразуем строку в дату
 
-  // Проверяем корректность целевой даты
-  if (isNaN(targetDate)) {
-    console.error("Invalid date format in data-timeTo attribute. Use YYYY-MM-DD.")
-    return
-  }
+  if (counter) {
+    const timeTo = counter.getAttribute("data-timeTo")
+    const targetDate = new Date(timeTo) // Преобразуем строку в дату
 
-  function updateCounter() {
-    const now = new Date()
-    const diff = targetDate - now
-
-    if (diff <= 0) {
-      // Если время истекло
-      document.getElementById("days").textContent = 0
-      document.getElementById("hours").textContent = 0
-      document.getElementById("minutes").textContent = 0
+    // Проверяем корректность целевой даты
+    if (isNaN(targetDate)) {
+      console.error("Invalid date format in data-timeTo attribute. Use YYYY-MM-DD.")
       return
     }
 
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+    function updateCounter() {
+      const now = new Date()
+      const diff = targetDate - now
 
-    document.getElementById("days").textContent = days
-    document.getElementById("hours").textContent = hours
-    document.getElementById("minutes").textContent = minutes
+      if (diff <= 0) {
+        // Если время истекло
+        document.getElementById("days").textContent = 0
+        document.getElementById("hours").textContent = 0
+        document.getElementById("minutes").textContent = 0
+        return
+      }
+
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+      const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+
+      document.getElementById("days").textContent = days
+      document.getElementById("hours").textContent = hours
+      document.getElementById("minutes").textContent = minutes
+    }
+
+    setInterval(updateCounter, 1000)
+    updateCounter()
   }
-
-  setInterval(updateCounter, 1000)
-  updateCounter()
 }
